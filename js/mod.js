daadf2e9fcd10a9db1ad6a6d6215009bcdc13831
wowtown <bigmitchdawg@gmail.com>
2022-02-05 16:49:12 +1100

    Update mod.js
    did very little

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Wonderful Tree",
+	id: "wonderful",
+	author: "wowtown",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
